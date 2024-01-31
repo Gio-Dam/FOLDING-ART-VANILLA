5afcf8d0ff692f2231194d9e852fd39c0409eb3e
wiizz <wiizz76.contact@gmail.com>
2024-01-31 22:13:36 +0100

    fix: year auto incrementation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../css/Footer.css";
 export function Footer() {
     return (
         <div className="footer-container">
-            <p>COPYRIGHT © 2024 FOLDING ART</p>
+            <p>COPYRIGHT © {new Date().getFullYear()} FOLDING ART</p>
         </div>
     );
 }
